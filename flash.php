43f532019e40b58d93408dd2ea318bc758b6a71b
David Rimbault <rimbault.david@gmail.com>
2016-04-12 10:44:37 +0200

    [ADD] Nouvelle Maquette

---
+++ flash.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <?php include ('inc/head.php') ?>
+
+</head>
+<body class="pageblanche" id="holder" style="background:#dddddd;">
+    
+    <div id="close"><a href="carte.php">x</a></div>
+    <div class="container">
+      <div class="row">
+          <div class="col-xs-6">
+            <a href="qrcode.php"><img class="Photo" src="images/barquette1.jpg" alt="..." /></a>
+          </div>
+          <div class="col-xs-6">
+            <a href="qrcode.php"><img class="Photo" src="images/barquette2.jpg" alt="..." /></a>
+          </div>
+          <div class="col-xs-6">
+            <a href="qrcode.php"><img class="Photo" src="images/barquette3.jpg" alt="..." /></a>
+          </div>
+          <div class="col-xs-6">
+            <a href="qrcode.php"><img class="Photo" src="images/barquette4.jpg" alt="..." /></a>
+          </div>
+      </div>
+      
+    </div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
