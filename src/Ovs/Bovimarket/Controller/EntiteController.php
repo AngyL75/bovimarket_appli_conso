8820fe549d23fad78ca6aa37b17ae5e684c99c8b
David Rimbault <rimbault.david@gmail.com>
2016-09-30 11:48:20 +0200

    [UPD] Fiche Pro

--- src/Ovs/Bovimarket/Controller/EntiteController.php
+++ src/Ovs/Bovimarket/Controller/EntiteController.php
@@ -10,26 +10,82 @@ namespace Ovs\Bovimarket\Controller;
 
 use Ovs\Bovimarket\Entities\Api\Certification;
 use Ovs\Bovimarket\Entities\Api\Entite;
+use Ovs\Bovimarket\Entities\Interfaces\Collection;
 use Ovs\Bovimarket\Services\API\BilletFetcherService;
 use Ovs\Bovimarket\Services\API\CertificationFetcherService;
 use Ovs\Bovimarket\Services\Api\EntiteFetcherService;
 use Ovs\Bovimarket\Services\API\MenuFetcherService;
 use Ovs\Bovimarket\Utils\Region;
+use Ovs\Bovimarket\Utils\Utils;
 use Ovs\SlimUtils\Controller\BaseController;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
 class EntiteController extends BaseController
 {
+
+	private function getCertifications(Entite $entite){
+		$certifs=null;
+		$idsCertif = array();
+		/** @var CertificationFetcherService $certifFetcher */
+		$certifFetcher = $this->get('certifications');
+
+
+		foreach ($entite->getCertifications() as $certification) {
+			if ($certification['valide']) {
+				$idsCertif[] = $certification['certificationId'];
+			}
+		}
+
+		if (count($idsCertif) > 0) {
+			$retApi = $certifFetcher->getApi()->get('certifications/liste', array(
+				'query' => array(
+					'ids' => implode(',', $idsCertif),
+				),
+			));
+			$body = (string) $retApi->getBody();
+			$certifs = $certifFetcher->unserialize($body, Certification::class);
+		}
+
+		return $certifs;
+	}
+
+	private function getMembres($filiere){
+		/** @var CertificationFetcherService $certifFetcher */
+		$certifFetcher = $this->get('certifications');
+		/** @var EntiteFetcherService $entiteFetcher */
+		$entiteFetcher = $this->get('entites');
+
+		$certifications = $certifFetcher->getApi()->get("certifications",array(
+			"query"=>array(
+				"entiteId"=>$filiere->getId()
+			)
+		));
+		$body = (string)$certifications->getBody();
+		$certifications = $certifFetcher->unserialize($body);
+		$certifsIds = $certifications->getKeys();
+
+		$membres = $entiteFetcher->getApi()->get("entites",array(
+			"query"=>array(
+				"certificationsIds"=>implode(",",$certifsIds)
+			)
+		));
+		$body = (string)$membres->getBody();
+		$membres = $entiteFetcher->unserialize($body);
+
+		return $membres;
+	}
+
     public function showDetailAction(Request $request, Response $response, $args)
     {
-        $certifs = null;
-
+	    $membres=array();
+	    /** @var EntiteFetcherService $entiteFetcher */
+	    $entiteFetcher = $this->get('entites');
         /** @var Entite $entite */
-        $entite = $this->get('entites')->find($args['id']);
+        $entite = $entiteFetcher->find($args['id']);
+
         /** @var MenuFetcherService $menuFetcher */
         $menuFetcher = $this->get('menus');
-
         $menuFetcher->setEndpointParams(array('entiteId' => $args['id']));
         $menus = $menuFetcher->findAll();
 
@@ -37,24 +93,11 @@ class EntiteController extends BaseController
         if ($entite->getActivite() == 'RESTAURANT' || $entite->getActivite() == 'RESTAURATION_COLLECTIVE') {
             $template = 'Entites/detail_restaurant.html.twig';
         }
+        $certifs = $this->getCertifications($entite);
 
-        $idsCertif = array();
-        foreach ($entite->getCertifications() as $certification) {
-            if ($certification['valide']) {
-                $idsCertif[] = $certification['certificationId'];
-            }
-        }
 
-        if (count($idsCertif) > 0) {
-            /** @var CertificationFetcherService $certifFetcher */
-            $certifFetcher = $this->get('certifications');
-            $retApi = $certifFetcher->getApi()->get('certifications/liste', array(
-                'query' => array(
-                    'ids' => implode(',', $idsCertif),
-                ),
-            ));
-            $body = (string) $retApi->getBody();
-            $certifs = $certifFetcher->unserialize($body, Certification::class);
+        if($entite->getActivite() == "FILIERE"){
+        	$membres=$this->getMembres($entite);
         }
 
         /** @var BilletFetcherService $billetFetcher */
@@ -70,6 +113,7 @@ class EntiteController extends BaseController
             'certifs' => $certifs,
             'blog' => $billets,
             'presse' => $presse,
+	        'membres'=>$membres
         ));
     }
 
